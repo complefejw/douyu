f63c439d1f269dbc665f1f34dc863ff07555d41a
xiaonan <wttgold8@gmail.com>
2021-03-19 19:17:07 +0800

    初始化创建

---
+++ douyu/douyu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  douyu
+//
+//  Created by x on 2021/3/19.
+//  Copyright © 2021 x. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
